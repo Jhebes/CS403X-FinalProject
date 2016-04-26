6127e8754e9180bd5a9d7791468bcc027d9cb444
Anthony J. Ruffa <ajosephruffa@gmail.com>
2016-04-25 22:27:47 -0400

    got hamburger menu working

---
+++ app/src/main/java/trendly/cs403x/trendly/MainFragment.java
@@ -0,0 +1,30 @@
+package trendly.cs403x.trendly;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+}
