c1ac659d8e926ebd13a246d509b3b7dcc18cbaef
Anthony J. Ruffa <ajosephruffa@gmail.com>
2016-04-27 16:00:25 -0400

    added favorites and cart in top right menu

--- app/src/main/java/trendly/cs403x/trendly/MainActivity.java
+++ app/src/main/java/trendly/cs403x/trendly/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_favorites || id == R.id.action_cart) {
             return true;
         }
 
