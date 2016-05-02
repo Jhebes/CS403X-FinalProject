f514883873e2edaced165a99f001235bc2ad5d09
Anthony J. Ruffa <ajosephruffa@gmail.com>
2016-05-02 18:18:33 -0400

    added server calls for registering likes and getting cards

--- app/src/main/java/trendly/cs403x/trendly/MyFragment.java
+++ app/src/main/java/trendly/cs403x/trendly/MyFragment.java
@@ -1,6 +1,8 @@
 package trendly.cs403x.trendly;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -8,11 +10,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import butterknife.ButterKnife;
@@ -23,27 +34,103 @@ import butterknife.OnClick;
 public class MyFragment extends Fragment {
 
     private ArrayList<String> al;
+    private ArrayList<String> idArray;
     private ArrayAdapter<String> arrayAdapter;
-    private int i;
 
     @InjectView(R.id.frame) SwipeFlingAdapterView flingContainer;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        al = new ArrayList<>();
+        idArray = new ArrayList<>();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_my, container, false);
         ButterKnife.inject(this, view);
 
-        al = new ArrayList<>();
-        al.add("php");
-        al.add("c");
-        al.add("python");
-        al.add("java");
-        al.add("html");
-        al.add("c++");
-        al.add("css");
-        al.add("javascript");
+        getJSONList("nearby", "56", "90", "20", "5");
 
+        return view;
+    }
+
+    static void makeToast(Context ctx, String s){
+        Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getJSONList(final String reqType, final String lat, final String lng, final String range, final String count) {
+        new AsyncTask<Void, Void, String>() {
+            ProgressDialog progressDialog = new ProgressDialog(getContext(), ProgressDialog.STYLE_SPINNER);
+
+            @Override
+            protected void onPreExecute() {
+                progressDialog.setTitle("Please wait");
+                progressDialog.setMessage("Retrieving items...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                progressDialog.setCanceledOnTouchOutside(false);
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                URL url;
+                HttpURLConnection connection = null;
+                String response;
+
+                try {
+                    String requestJson = "{\"reqType\":\"" + reqType
+                            + "\", \"lat\":\"" + lat + "\", \"lng\":\"" + lng +
+                            "\", \"range\":\"" + range + "\", \"count\":\"" + count + "\"}";
+                    url = new URL("http://murmuring-depths-93571.herokuapp.com/rest");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setConnectTimeout(30000);
+                    connection.setReadTimeout(30000);
+                    connection.setDoOutput(true);
+
+                    DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+                    outputStream.writeBytes(requestJson);
+                    outputStream.flush();
+                    outputStream.close();
+
+                    response = readResponse(new BufferedInputStream(connection.getInputStream()));
+                    return response;
+                } catch (Exception e) {
+                    Log.d("MyFragment", "Could not retrieve items.");
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                progressDialog.dismiss();
+                try {
+                    JSONArray resultArray = new JSONArray(s);
+
+                    for (int i = 0; i < resultArray.length(); i++) {
+                        JSONObject jsonObject = resultArray.getJSONObject(i);
+                        String itemName = jsonObject.getString("itemName");
+                        String itemId = jsonObject.getString("item");
+                        al.add(itemName);
+                        idArray.add(itemId);
+                    }
+                    setCardView();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.execute();
+    }
+
+    private void setCardView() {
         arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.item, R.id.helloText, al);
-
 
         flingContainer.setAdapter(arrayAdapter);
         flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
@@ -52,6 +139,7 @@ public class MyFragment extends Fragment {
                 // this is the simplest way to delete an object from the Adapter (/AdapterView)
                 Log.d("LIST", "removed object!");
                 al.remove(0);
+                idArray.remove(0);
                 arrayAdapter.notifyDataSetChanged();
             }
 
@@ -60,21 +148,18 @@ public class MyFragment extends Fragment {
                 //Do something on the left!
                 //You also have access to the original object.
                 //If you want to use it just cast it (String) dataObject
-                makeToast(getActivity(), "Left!");
+
             }
 
             @Override
             public void onRightCardExit(Object dataObject) {
-                makeToast(getActivity(), "Right!");
+                sendLikeToServer(idArray.get(0), "56", "90");
             }
 
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
                 // Ask for more data here
-                al.add("XML ".concat(String.valueOf(i)));
-                arrayAdapter.notifyDataSetChanged();
-                Log.d("LIST", "notified");
-                i++;
+                getJSONList("nearby", "56", "90", "20", "5");
             }
 
             @Override
@@ -93,12 +178,6 @@ public class MyFragment extends Fragment {
                 makeToast(getActivity(), "Clicked!");
             }
         });
-
-        return view;
-    }
-
-    static void makeToast(Context ctx, String s){
-        Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
     }
 
 
@@ -115,5 +194,84 @@ public class MyFragment extends Fragment {
         flingContainer.getTopCardListener().selectLeft();
     }
 
+    public static String readResponse(InputStream stream) {
+        StringBuilder result = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        try {
+            // Store the response as a string.
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+            reader.close();
+        } catch (Exception e) {
+            Log.v("ServerProblemHelper", "Error reading response.");
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
 
+    private void sendLikeToServer(final String itemId, final String lat, final String lng) {
+        new AsyncTask<Void, Void, String>() {
+            ProgressDialog progressDialog = new ProgressDialog(getContext(), ProgressDialog.STYLE_SPINNER);
+
+            @Override
+            protected void onPreExecute() {
+                progressDialog.setTitle("Please wait");
+                progressDialog.setMessage("Sending like...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                progressDialog.setCanceledOnTouchOutside(false);
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                URL url;
+                HttpURLConnection connection = null;
+                String response;
+
+                try {
+                    String requestJson = "{\"reqType\":\"addlike\", \"itemId\":\"" + itemId
+                            + "\", \"lat\":\"" + lat + "\", \"lng\":\"" + lng + "\"}";
+                    url = new URL("http://murmuring-depths-93571.herokuapp.com/rest");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setConnectTimeout(30000);
+                    connection.setReadTimeout(30000);
+                    connection.setDoOutput(true);
+
+                    DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+                    outputStream.writeBytes(requestJson);
+                    outputStream.flush();
+                    outputStream.close();
+
+                    response = readResponse(new BufferedInputStream(connection.getInputStream()));
+                    return response;
+                } catch (Exception e) {
+                    Log.d("MyFragment", "Could not send like to server.");
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                progressDialog.dismiss();
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    String result = jsonObject.getString("result");
+                    if ("Ok".equals(result)) {
+                        makeToast(getActivity(), "Like registered");
+                    } else {
+                        makeToast(getActivity(), "An error occurred. Please try again later.");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.execute();
+    }
 }
